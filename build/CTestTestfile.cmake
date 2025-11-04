b2e98ec2a870850f636919c50e82457b4a24fc1a
richardzang2006-svg <richardzang2006@gmail.com>
2025-11-04 16:34:18 +0800

    第一次测试是否能访问图片

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/LIMITI-vision
+# Build directory: /root/LIMITI-vision/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyTest "/root/LIMITI-vision/build/run")
+set_tests_properties(MyTest PROPERTIES  _BACKTRACE_TRIPLES "/root/LIMITI-vision/CMakeLists.txt;20;add_test;/root/LIMITI-vision/CMakeLists.txt;0;")
--- test.cpp
+++ test.cpp
@@ -1,5 +1,21 @@
-int main()
-{
-int a = 5;
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main() {
+    // 读取图片
+    cv::Mat image = cv::imread("/root/LIMITI-vision/example.png");
+
+    // 检查图片是否成功加载
+    if (image.empty()) {
+        std::cout << "无法加载图片！" << std::endl;
+        return -1;
+    }
+
+    // 显示图片
+    cv::imshow("Display Image", image);
+
+    // 等待按键
+    cv::waitKey(0);
+
     return 0;
 }
