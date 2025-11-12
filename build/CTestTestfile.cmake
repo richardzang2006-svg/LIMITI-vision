079d85529cf781f477f837ad00f06a98f49c0704
richardzang2006 <richardzang2006@gmail.com>
2025-11-12 22:26:18 +0800

    .

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/vscodeprojects/LIMITI-vision
-# Build directory: /home/workspace/vscodeprojects/LIMITI-vision/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyTest "/home/workspace/vscodeprojects/LIMITI-vision/build/run")
-set_tests_properties(MyTest PROPERTIES  ENVIRONMENT "DISPLAY=:0;LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/local/lib" _BACKTRACE_TRIPLES "/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;33;add_test;/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;0;")
