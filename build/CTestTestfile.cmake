b966ca32c980d8eabca81bc8f6edee7c8654f005
richardzang2006 <richardzang2006@gmail.com>
2025-11-12 16:25:24 +0800

    在ubuntu24.04实机上运行

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /root/LIMITI-vision
-# Build directory: /root/LIMITI-vision/build
+# Source directory: /home/workspace/vscodeprojects/LIMITI-vision
+# Build directory: /home/workspace/vscodeprojects/LIMITI-vision/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(MyTest "/root/LIMITI-vision/build/run")
-set_tests_properties(MyTest PROPERTIES  _BACKTRACE_TRIPLES "/root/LIMITI-vision/CMakeLists.txt;20;add_test;/root/LIMITI-vision/CMakeLists.txt;0;")
+add_test(MyTest "/home/workspace/vscodeprojects/LIMITI-vision/build/run")
+set_tests_properties(MyTest PROPERTIES  ENVIRONMENT "DISPLAY=:99;LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/local/lib" _BACKTRACE_TRIPLES "/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;33;add_test;/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;0;")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /root/LIMITI-vision
+# Install script for directory: /home/workspace/vscodeprojects/LIMITI-vision
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/root/LIMITI-vision/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/workspace/vscodeprojects/LIMITI-vision/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
