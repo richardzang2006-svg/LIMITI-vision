d319c68acdacde912fb2164dae1710120277e5d8
richardzang2006 <richardzang2006@gmail.com>
2025-11-12 22:19:35 +0800

    实验gitignore

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(MyTest "/home/workspace/vscodeprojects/LIMITI-vision/build/run")
-set_tests_properties(MyTest PROPERTIES  ENVIRONMENT "DISPLAY=:99;LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/local/lib" _BACKTRACE_TRIPLES "/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;33;add_test;/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;0;")
+set_tests_properties(MyTest PROPERTIES  ENVIRONMENT "DISPLAY=:0;LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:/usr/local/lib" _BACKTRACE_TRIPLES "/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;33;add_test;/home/workspace/vscodeprojects/LIMITI-vision/CMakeLists.txt;0;")
